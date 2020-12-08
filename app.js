cfbee2bb9c888e4c66c20c6c112f2b125762254b
Jesus Garcia <jesus1287garcia@yahoo.com>
2020-12-08 11:57:27 -0800

    added random number gen for 3 numb only

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const game = ()=> {
     let pScore = 0;
     let cScore = 0;
 
+    //start the game
     const startGame = ()=> {
         const PlayBtn = document.querySelector('.intro button');
         const introScreen = document.querySelector('.intro');
@@ -9,13 +10,26 @@ const game = ()=> {
         
         PlayBtn.addEventListener('click', () =>{
             introScreen.classList.add('fadeOut');
+            match.classList.add("fadeIn");
         });
     };
+    //play match
+    const playMatch = () => {
+        const options = document.querySelectorAll('.options button');
+        const playerHand = document.querySelector('.player-hand');
+        const computerHand = document.querySelector('.computer-hand');
+        //computer otions
+        const computerOtions = ['rock', 'paper', 'scissors'];
+
+        const computerNumber = Math.floor(Math.random() * 3);
+        
+    }
 
     //Is call all the inner function
     startGame();
-    updateScore();
+    playMatch();
 };
 
 
 //start the game funsction
+game();
