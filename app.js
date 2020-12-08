5f6291a326482da0551d56ec1d3fd3fd1f52543e
Jesus Garcia <jesus1287garcia@yahoo.com>
2020-12-08 12:51:15 -0800

    compareing hand

--- app.js
+++ app.js
@@ -18,12 +18,69 @@ const game = ()=> {
         const options = document.querySelectorAll('.options button');
         const playerHand = document.querySelector('.player-hand');
         const computerHand = document.querySelector('.computer-hand');
-        //computer otions
-        const computerOtions = ['rock', 'paper', 'scissors'];
+        //computer options
+        const computerOptions = ['rock', 'paper', 'scissors'];
 
-        const computerNumber = Math.floor(Math.random() * 3);
+        options.forEach(option => {
+            option.addEventListener('click', function() {
+                //computer choice
+                const computerNumber = Math.floor(Math.random() * 3);
+                const computerChoice = computerOptions[computerNumber];
+                // call compare hands
+            });
+        }); 
         
-    }
+    };
+
+    const comparehands = (playerChoice, computerChoice) =>{
+        //update texts
+        const winner = document.querySelector('.winner')
+        //checking 4 a tie
+        if(playerChoice === computerChoice) {
+            winner.textContent = 'Its a tie';
+            return;
+        }
+        //check 4 rock
+        if(playerChoice === 'rock'){
+            if(computerChoice === 'scissors') {
+                winner.textContent = 'Player Wins'
+                return;
+            }else{
+                winner.textContent = 'Computer Wins';
+                return;
+            }
+        }
+        //check 4 paper
+        if(playerChoice === 'paper'){
+            if(computerChoice === 'scissors') {
+                winner.textContent = 'Computer Wins'
+                return;
+            }else{
+                winner.textContent = 'Player Wins';
+                return;
+            }
+        }
+        // check 4 scissors
+        if(playerChoice === 'scissors'){
+            if(computerChoice === 'scissors') {
+                winner.textContent = 'Computer Wins'
+                return;
+            }else{
+                winner.textContent = 'Player Wins';
+                return;
+            }
+        }
+        //check for scissors
+        if(playerChoice === 'scissors'){
+            if(computerChoice === 'rock') {
+                winner.textContent = 'Computer Wins'
+                return;
+            }else{
+                winner.textContent = 'Player Wins';
+                return;
+            }
+        }
+    } 
 
     //Is call all the inner function
     startGame();
